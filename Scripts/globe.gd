757f4586a52b03de3404f7044acf3e7b14d35c3e
Warren G <magmamutt0@gmail.com>
2026-07-27 23:34:44 -0400

    Added an end state and added the font to all the games.

--- Scripts/globe.gd
+++ Scripts/globe.gd
@@ -1,8 +1,11 @@
 extends Control
-
+@onready var main_node = get_tree().current_scene
 
 func _ready() -> void:
-	$AnimatedSprite2D.play("default")
+	if main_node.name == "Victory":
+		$AnimatedSprite2D.play("dirtiest")
+	else:
+		$AnimatedSprite2D.play("clean")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scripts/main_game_scene.gd
+++ Scripts/main_game_scene.gd
@@ -9,14 +9,14 @@ var score = 0
 var microgamesEasy = [
 	#preload("res://Scenes/Microgames/PressButton.tscn"),
 	#preload("res://Scenes/Microgames/BluePressButton.tscn"),
-	preload("res://Scenes/Microgames/Trickshot.tscn"),
+	#preload("res://Scenes/Microgames/Trickshot.tscn"),
 	preload("res://Scenes/Microgames/Commuting.tscn"),
-	preload("res://Scenes/Microgames/Fishing.tscn"),
+	#preload("res://Scenes/Microgames/Fishing.tscn"),
 	preload("res://Scenes/Microgames/Fracking.tscn"),
-	preload("res://Scenes/Microgames/Consuming.tscn"),
-	preload("res://Scenes/Microgames/Sweating.tscn"),
-	preload("res://Scenes/Microgames/Shining.tscn"),
-	preload("res://Scenes/Microgames/Burning.tscn")
+	#preload("res://Scenes/Microgames/Consuming.tscn"),
+	#preload("res://Scenes/Microgames/Sweating.tscn"),
+	#preload("res://Scenes/Microgames/Shining.tscn"),
+	#preload("res://Scenes/Microgames/Burning.tscn")
 ]
 var microgamesMid = [
 	preload("res://Scenes/Microgames/Trickshot.tscn"),
@@ -25,7 +25,7 @@ var microgamesMid = [
 	preload("res://Scenes/Microgames/Commuting.tscn"),
 	preload("res://Scenes/Microgames/Fishing.tscn"),
 	preload("res://Scenes/Microgames/Fracking.tscn"),
-	preload("res://Scenes/Microgames/Consuming.tscn"),
+	#preload("res://Scenes/Microgames/Consuming.tscn"),
 	preload("res://Scenes/Microgames/Sweating.tscn"),
 	preload("res://Scenes/Microgames/Sawing.tscn"),
 	preload("res://Scenes/Microgames/Burning.tscn")
@@ -36,7 +36,7 @@ var microgamesHard = [
 	preload("res://Scenes/Microgames/Fishing.tscn"),
 	preload("res://Scenes/Microgames/Hosing.tscn"),
 	preload("res://Scenes/Microgames/Fracking.tscn"),
-	preload("res://Scenes/Microgames/Consuming.tscn"),
+	#preload("res://Scenes/Microgames/Consuming.tscn"),
 	preload("res://Scenes/Microgames/Sweating.tscn"),
 	preload("res://Scenes/Microgames/Sawing.tscn")
 ]
@@ -64,7 +64,7 @@ func game_loop():
 
 #Function to play a microgame
 func play_microgame():
-	if microgames_left == 0:
+	if (microgames_left == 0):
 		difficulty += 1
 		# Changing the list of current available microgames
 		match difficulty:
@@ -107,6 +107,8 @@ func play_microgame():
 	if microgame.did_win:
 		print("WIN")
 		score += 1
+		if score >= 20:
+			get_tree().change_scene_to_file("res://Scenes/Victory.tscn")
 		print(score)
 		lost = false
 		microgames_left -= 1
