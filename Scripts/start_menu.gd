ead48acedbb25d586f982b7d8255e504c984d986
worldwarwarren <magmamutt0@gmail.com>
2026-08-11 16:44:35 -0400

    Credits

--- Scripts/start_menu.gd
+++ Scripts/start_menu.gd
@@ -40,3 +40,8 @@ func _on_quit_button_pressed() -> void:
 
 func _on_tutorial_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Tutorial.tscn")
+	
+
+
+func _on_credits_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Credits.tscn")
